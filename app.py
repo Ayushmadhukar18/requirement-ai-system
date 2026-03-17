305f8095e7c06e3184fd345c43981e5a53255003
Ayushmadhukar18 <ayushmadhukar05@gmail.com>
2026-03-17 11:18:43 +0530

    merger conflits resolved

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from api.routes import router
 from dotenv import load_dotenv
 from pathlib import Path
 
-<<<<<<< HEAD
 # Load environment variables from .env in the project root
 env_path = Path(__file__).resolve().parent / ".env"
 load_dotenv(dotenv_path=env_path)
--- services/task_planner.py
+++ services/task_planner.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 import os
-<<<<<<< HEAD
+
 from dotenv import load_dotenv
 
 load_dotenv()
